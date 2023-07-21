032fc63051a62379ee6709da9545e32898ee557a
Rafael Lemos <contact@flemosr.com>
2023-07-21 09:13:37 -0300

    envoy: add copyright notice to modified `docker-entrypoint.sh`

--- envoy/docker-entrypoint.sh
+++ envoy/docker-entrypoint.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env sh
+
+# Copyright 2023 Envoy Project Authors
+# Modifications Copyright 2023 Rafael Lemos
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -e
 
-# generate envoy.yaml from template and env variables ##########################
+# Generate envoy.yaml from template and env variables ##########################
 
 envsubst '
     $SERVER_PORT
@@ -14,7 +30,7 @@ envsubst '
 
 chmod go+r /etc/envoy/envoy.yaml
 
-# standard entrypoint ##########################################################
+# Original entrypoint ##########################################################
 
 loglevel="${loglevel:-}"
 USERID=$(id -u)
